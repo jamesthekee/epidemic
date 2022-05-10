94f7c6384e381d3603dbca083b6f26b85589b5e6
james kee <43153742+jamesthekee@users.noreply.github.com>
2022-05-10 23:39:26 +0100

    Cleaned up class Added vaccination

--- network_sir.py
+++ network_sir.py
@@ -1,104 +1,142 @@
+import math
 import numpy as np
 import networkx as nx
-import numpy.random
 import random
 import pandas
 import matplotlib.pyplot as plt
-
+import scipy
+from functools import lru_cache
 
 
 SUSCEPTIBLE_STATE = 0
 INFECTED_STATE = 1
 REMOVED_STATE = 2
+VACCINATED_STATE = 3
+EXPOSED_STATE = 4
 
-class network_SIR:
 
-    def __init__(self, network, trans, recover, immunity_loss=0):
-        self.network = network
-        self.total_population = len(network.nodes)
+class Population:
+    def __init__(self, size):
+        self.total_population = size
         self.population = [SUSCEPTIBLE_STATE for _ in range(self.total_population)]
-        self.trans = trans
-        self.recover = recover
-        self.imm_loss = immunity_loss
 
-    def reset(self):
+    def reset(self, reset_vaccinated=False):
+        if reset_vaccinated:
+            for i in range(self.total_population):
+                self.population[i] = SUSCEPTIBLE_STATE
+            return
+
         for i in range(self.total_population):
-            self.population = SUSCEPTIBLE_STATE
+            if self.population[i] != VACCINATED_STATE:
+                self.population[i] = SUSCEPTIBLE_STATE
 
-    def begin_infection(self, init):
-        initial_infected = numpy.random.randint(0, self.total_population, init)
+    def begin_epidemic(self, init):
+        initial_infected = np.random.choice(self.total_population, size=init, replace=False)
         cur = set()
         for i in initial_infected:
             self.population[i] = INFECTED_STATE
             cur.add(i)
         return cur
 
-    def SIR_step(self, cur_infected):
+    def count_type(self, type):
+        total = 0
+        for x in self.population:
+            if x == type:
+                total += 1
+        return total
+
+
+class network_SIR:
+
+
+    def __init__(self, network, infect_rate, recover_rate, immunity_loss=0):
+
+
+        if not nx.is_connected(network):
+            raise ValueError("Provided network is not connected")
+
+        for k in [infect_rate, recover_rate]:
+            if not 0 < infect_rate <= 1:
+                raise ValueError(f"{k.__name__} is not in interval (0, 1]")
+
+        self.network = network
+        self.total_population = len(network.nodes)
+        self.population = Population(self.total_population)
+        self.infect_rate = infect_rate
+        self.recover_rate = recover_rate
+        self.immunity_loss = immunity_loss
+
+    def reset(self, reset_vaccinated=False):
+        self.population.reset(reset_vaccinated=reset_vaccinated)
+
+    def _SIR_step(self, cur_infected):
+        # S->I : get infected
         new_infected = set()
         for i in cur_infected:
             for neighbor in nx.neighbors(self.network, i):
-                if self.population[neighbor] == SUSCEPTIBLE_STATE \
-                    and random.random() < self.trans:
+                if self.population.population[neighbor] == SUSCEPTIBLE_STATE \
+                    and random.random() < self.infect_rate:
                     new_infected.add(neighbor)
-                    self.population[neighbor] = INFECTED_STATE
+                    self.population.population[neighbor] = INFECTED_STATE
 
-        # Loose immunisiation
-        for i in range(self.total_population):
-            if self.population[i] == REMOVED_STATE and random.random() < self.imm_loss:
-                self.population[i] = SUSCEPTIBLE_STATE
+        # R->S : loose immunity
+        if self.immunity_loss:
+            for i in range(self.total_population):
+                if self.population.population[i] == REMOVED_STATE and random.random() < self.immunity_loss:
+                    self.population.population[i] = SUSCEPTIBLE_STATE
 
+        # I->R : recover from infection
         new_removed = set()
         for i in cur_infected:
-            if random.random() < self.recover:
+            if random.random() < self.recover_rate:
                 new_removed.add(i)
-                self.population[i] = REMOVED_STATE
+                self.population.population[i] = REMOVED_STATE
 
         cur_infected = cur_infected.difference(new_removed)
         cur_infected = cur_infected.union(new_infected)
 
         return cur_infected
 
-    def epidemic_to_completion(self):
-        if self.imm_loss > 0:
-            raise ValueError("oops")
+    def epidemic(self, initial_infection=5, step_limits=None):
+        if not step_limits:
+            step_limits = float("inf")
 
         infected_data = []
-        cur = self.begin_infection(5)
-        infected_data.append(len(cur))
-        while cur:
-            cur = self.SIR_step(cur)
-            infected_data.append(len(cur))
-        return infected_data
-
-    def epidemic_steps(self, n):
-        infected_data = []
-        cur = self.begin_infection(5)
+        cur = self.population.begin_epidemic(initial_infection)
         infected_data.append(len(cur))
 
         i = 0
-        while cur and i < n:
-            cur = self.SIR_step(cur)
+        while cur and i < step_limits:
+            cur = self._SIR_step(cur)
             infected_data.append(len(cur))
             i += 1
         return infected_data
 
-    def get_total_infected(self):
-        total = 0
-        for x in self.population:
-            if x ==1 or x == 2:
-                total += 1
-        return total
+    def random_vaccinate(self, prop):
+        # TODO: Do later lol
+        pass
+        count = int(self.total_population*prop)
+        victims = np.random.choice(self.total_population, count, replace=False)
+        for i in victims:
+            self.population.population[i] = VACCINATED_STATE
+
+    def get_count(self, x):
+        return self.population.count_type(x)
 
 
 if __name__ == "__main__":
-    network = nx.generators.watts_strogatz_graph(5000, 6, 0.15)
-    # network = nx.generators.barabasi_albert_graph(10000, 3)
+    graph_properties = dict(size=1000,
+                            average_degree=6,
+                            random_mixing=0.15)
+    network = nx.generators.watts_strogatz_graph(graph_properties["size"], graph_properties["average_degree"], graph_properties["random_mixing"])
+    sim_properties = dict(network=network,
+                          infect_rate=0.18,
+                          recover_rate=0.25,
+                          immunity_loss=0.024)
 
-    sim = network_SIR(network, 0.18, 0.25, immunity_loss=0.022)
-    data = numpy.array(sim.epidemic_steps(2500))
-
-    data = data / 5000
-    # print(sim.get_total_infected())
+    sim = network_SIR(**sim_properties)
+    data = np.array(sim.epidemic(step_limits=2500))
+    data = data / graph_properties["size"]
     plt.plot(data)
     plt.show()
 
