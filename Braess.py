f20bdd69d19729b21c3f4c0213ba86c64e74ad03
james kee <43153742+jamesthekee@users.noreply.github.com>
2022-10-26 21:30:44 +0100

    Add some basic comments

---
+++ Braess.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Created By  : James Kee
+# Created Date: 26/10/2022
+# ---------------------------------------------------------------------------
+""" Recreating this experiment showing a Braess' paradox situation in voluntary vaccination
+Zhang et al, 'Braess’s Paradox in Epidemic Game'"""
